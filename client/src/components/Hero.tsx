ca5c2117fba85b537a1fe73c85ab57fae6737444
Sudharsan Srinivasan <sudharsanak1010@gmail.com>
2025-11-08 18:39:15 -0600

    Update Hero.tsx

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -36,7 +36,7 @@ export default function Hero() {
               <Button onClick={() => scrollToSection("projects")} size="lg" data-testid="button-view-projects">
                 View Projects
               </Button>
-              <Button variant="outline" size="lg" data-testid="button-download-resume">
+              <Button variant="outline" size="lg" data-testid="button-download-resume" as="a" href="/Sudharsan Srinivasan Resume 2025.pdf" download>
                 Download Resume
               </Button>
             </div>
