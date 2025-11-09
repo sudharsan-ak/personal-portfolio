237d899401da9381000bbf1b64c3c1d96cdc887c
Sudharsan Srinivasan <sudharsanak1010@gmail.com>
2025-11-09 13:38:09 -0600

    Update Projects.tsx

--- client/src/components/Projects.tsx
+++ client/src/components/Projects.tsx
@@ -7,12 +7,28 @@ export default function Projects() {
   const projects = [
     {
       title: "Personal Portfolio Website",
-      description: "Developed and deployed a responsive personal portfolio website to showcase projects.",
+      description:
+        "Developed and deployed a responsive personal portfolio website to showcase academic and professional projects. Integrated backend with PHPMyAdmin for dynamic content management and streamlined updates.",
       technologies: ["XAMPP", "MySQL", "HTML", "CSS", "PHP", "JavaScript"],
       year: "2020",
       githubUrl: "https://github.com/sudharsan-ak/Portfolio",
     },
-    // other projects ...
+    {
+      title: "Event Management System",
+      description:
+        "Built a web app for event booking and cancellation using Java Servlets and JSP. Automated test cases with Selenium IDE, achieving 100% statement coverage and PIT coverage.",
+      technologies: ["Java", "MySQL Workbench", "JUnit", "Selenium", "Eclipse"],
+      year: "2020",
+      githubUrl: "https://github.com/sudharsan-ak/CSE-6329-Catering-Management-System",
+    },
+    {
+      title: "Software Testing Using Java",
+      description:
+        "Implemented basis path testing with MCDC criteria, demonstrating advanced testing methodologies, code coverage, and QA practices for reliable software solutions.",
+      technologies: ["Java", "JUnit", "JaCoCo", "Pitclipse"],
+      year: "2019",
+      githubUrl: "",
+    },
   ];
 
   return (
