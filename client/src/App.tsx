c5e57fae8574b6fc1a493a45b910eb21cfff9bad
Sudharsan Srinivasan <sudharsanak1010@gmail.com>
2025-11-13 11:09:08 -0600

    Update App.tsx to comment out ai button for now

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -33,9 +33,10 @@ function App() {
           </div>
 
           {/* ✅ AI Assistant Button (Bottom-Right) */}
-          <div className="fixed bottom-6 right-6 z-50 hidden md:block">
+          {/* <div className="fixed bottom-6 right-6 z-50 hidden md:block">
             <AIAssistantButton />
           </div>
+          */}
         </DynamicBackground>
       </TooltipProvider>
     </QueryClientProvider>
