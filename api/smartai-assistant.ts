52b87c55ef7b38aa8c9478f97c9ce482c37695db
Sudharsan Srinivasan <sudharsanak1010@gmail.com>
2025-11-24 19:08:44 -0600

    Update smartai-assistant.ts

--- api/smartai-assistant.ts
+++ api/smartai-assistant.ts
@@ -1,4 +1,4 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { profileData } from "./profileData.js";
 
 // Related technologies for suggestions
